07ab97733457c38be7a88aeda5de03c91c384dec
Rondale W. Davis <rdavis2005@msn.com>
2018-04-25 21:12:10 +0000

    completed last modal updates

--- src/index.js
+++ src/index.js
@@ -7,51 +7,50 @@ import request from 'superagent';
 import './styles/app.css';
 
 class App extends React.Component {
-    constructor() {
-        super();
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            gifs: [],
-            selectedGif: null,
-            modalIsOpen: false
-        };
-        
-        this.handleTermChange = this.handleTermChange.bind(this);
-    }
+    this.state = {
+      gifs: [],
+      selectedGif: null,
+      modalIsOpen: false
+    };
+  }
 
-    openModal(gif) {
-        this.setState({
-            modalIsOpen: true,
-            selectedGif: gif
-        });
-    }
+  openModal(gif) {
+    this.setState({
+      modalIsOpen: true,
+      selectedGif: gif
+    });
+  }
 
-    closeModal() {
-        this.setState({
-            modalIsOpen: false,
-            selectedGif: null
-        });
-    }
+  closeModal() {
+    this.setState({
+      modalIsOpen: false,
+      selectedGif: null
+    });
+  }
 
-    handleTermChange(term) {
-        const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+  handleTermChange(term) {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
 
-        request.get(url, (err, res) => {
-            this.setState({ gifs: res.body.data })
-        });
-    }
-    
-    render() {
-        return (
-          <div>
-              <SearchBar onTermChange={this.handleTermChange} />
-              <GifList gifs={this.state.gifs} />
-              <GifModal modalIsOpen={this.state.modalIsOpen}
-                        selectedGif={this.state.selectedGif}
-                        onRequestClose={ () => this.closeModal() } />
-          </div>
-        );
-    }
+    request.get(url, (err, res) => {
+      this.setState({ gifs: res.body.data })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onTermChange={term => this.handleTermChange(term)} />
+        <GifList  gifs={this.state.gifs}
+                  onGifSelect={selectedGif => this.openModal(selectedGif) } />
+        <GifModal modalIsOpen={this.state.modalIsOpen}
+                  selectedGif={this.state.selectedGif}
+                  onRequestClose={ () => this.closeModal() } />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
